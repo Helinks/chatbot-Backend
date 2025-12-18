c3a03b2571cae5239e7c7fe0a7e2bbf64ba39a16
Helinks <130001483+Helinks@users.noreply.github.com>
2025-12-17 23:00:11 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/webhook', (req, res) => {
+   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  const entry = req.body.entry[0];
+  const event = entry.messaging[0];
+
+  const senderId = event.sender.id;
+  const message = event.message?.text;
+
+  if (message) {
+    enviarMensaje(senderId, `Recibí: ${message}`);
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo');
+});
