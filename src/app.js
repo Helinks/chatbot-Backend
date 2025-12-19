f20aaff28fafc85f12baf45af727d98151539a4f
Helinks <130001483+Helinks@users.noreply.github.com>
2025-12-19 14:40:01 -0500

    Soluciónde parentesis con express

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const webhookRoutes = require('./routes/webhook.routes');
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 app.use('/webhook',webhookRoutes);
 
