2441f72e309c4a1cb2547beb76ac9b756a6e0740
Helinks <130001483+Helinks@users.noreply.github.com>
2025-12-19 13:45:22 -0500

    Correcion del puerto

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>console.log('Bot Activo '))
+app.listen(PORT,'0.0.0.0',()=>console.log('Bot Activo '))
